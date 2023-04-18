9e3be46528fa97b379b22be6cf4d6da758e2ea8c
Manikanta <Manikanta662001@gmail.com>
2023-04-18 14:09:08 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -38,18 +38,6 @@ def display_webpage(request):
     return render(request,'display_webpage.html',d)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 def display_access(request):
     LOA=AccessRecord.objects.all()
     LOA=AccessRecord.objects.filter(date__gt='2022-01-12')
@@ -62,3 +50,41 @@ def display_access(request):
     LOA=AccessRecord.objects.filter(date__day='16')
     d={'access':LOA}
     return render(request,'display_access.html',d)
+
+
+def update_webpage(request):
+    #Webpage.objects.filter(name='MSD').update(email='dhoni@gmail.com')
+    #Webpage.objects.filter(email='dhoni@gmail.com').update(name='dhoni')
+    #Webpage.objects.update_or_create(name='dhoni',defaults={'url':'https://dhoni.in'})
+
+
+    T=Topic.objects.get_or_create(topic_name='cricket')[0]
+    T.save()
+    W=Webpage.objects.update_or_create(name='pandya',defaults={'topic_name':T,'url':'https://pandya.in','email':'pandya@gmail.com'})[0]
+    W.save()
+    d={'webpages':Webpage.objects.all()}
+    return render(request,'display_webpage.html',d)
+
+
+def delete_webpage(request):
+    #Webpage.objects.filter(name='Kohli').delete()
+    Webpage.objects.all().delete()
+    d={'webpages':Webpage.objects.all()}
+    return render(request,'display_webpage.html',d)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
